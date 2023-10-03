84724611b7b7a0ddc1bcf0bc94a1c1bb1b5c1211
ArtemRyz <RyzArtem@yandex.ru>
2023-10-03 16:13:22 +0500

    add

---
+++ tests/StringUtilsTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Hexlet\StringUtilsTest;
+
+use function Hexlet\StringUtils\capitalize;
+
+if (capitalize('hello') !== 'Hello') {
+    throw new \Exception('Функция работает неверно!');
+}
+
+if (capitalize('') !== '') {
+    throw new \Exception('Функция работает неверно!');
+}
+
+echo 'Все тесты пройдены!';
