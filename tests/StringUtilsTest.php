0e930e0961e2b0ed698dcb3887bb814507e25d59
ArtemRyz <RyzArtem@yandex.ru>
2023-10-03 22:51:45 +0500

    update

--- tests/StringUtilsTest.php
+++ tests/StringUtilsTest.php
@@ -4,7 +4,16 @@ namespace Hexlet\StringUtilsTest;
 
 use function Hexlet\StringUtils\capitalize;
 
-if (capitalize('hello') !== 'Hello') {
+$autoloadPath1 = __DIR__ . '/../../../autoload.php';
+$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
+
+if (file_exists($autoloadPath1)) {
+    require_once $autoloadPath1;
+} else {
+    require_once $autoloadPath2;
+}
+
+if (capitalize('... hello') !== '... Hello') {
     throw new \Exception('Функция работает неверно!');
 }
 
