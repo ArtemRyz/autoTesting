8ffa9b1a36e0343c149501f322026200d10085f2
ArtemRyz <RyzArtem@yandex.ru>
2023-10-05 17:33:58 +0500

    update

--- tests/StringUtilsTest.php
+++ tests/StringUtilsTest.php
@@ -2,9 +2,9 @@
 
 namespace Hexlet\StringUtilsTest;
 
-use function Hexlet\StringUtils\capitalize;
-
 use Webmozart\Assert\Assert;
+
+use function Hexlet\StringUtils\capitalize;
 
 $autoloadPath1 = __DIR__ . '/../../../autoload.php';
 $autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
